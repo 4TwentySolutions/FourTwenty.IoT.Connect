e0bc558a711f68947757c5660467756d5a737fc2
Pavel Kurianov <kurianov.pavel@gmail.com>
2019-12-24 22:44:56 +0200

    Revoked changes

--- FourTwenty.IoT.Connect/Models/Network/GrowPackage.cs
+++ FourTwenty.IoT.Connect/Models/Network/GrowPackage.cs
@@ -11,10 +11,36 @@ namespace FourTwenty.IoT.Connect.Models
         {
             Data = data;
         }
+
+        public GrowPackage(Uri host, T data) : base(host)
+        {
+            Data = data;
+        }
+
+        public GrowPackage(string host, T data) : base(host)
+        {
+            Data = data;
+        }
     }
 
     public class GrowPackage
     {
         public ModuleType ModuleType { get; set; }
+        public Uri Host { get; set; }
+
+        public GrowPackage()
+        {
+
+        }
+
+        public GrowPackage(string host)
+        {
+            Host = new Uri(host);
+        }
+
+        public GrowPackage(Uri host)
+        {
+            Host = host;
+        }
     }
 }
