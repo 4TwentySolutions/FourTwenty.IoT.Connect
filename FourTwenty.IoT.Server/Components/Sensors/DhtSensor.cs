4c39f9942f246288c1996fd64e12458edd1a8295
Pavel Kurianov <kurianov.pavel@gmail.com>
2020-02-10 00:20:47 +0200

    Add ctor

--- FourTwenty.IoT.Server/Components/Sensors/DhtSensor.cs
+++ FourTwenty.IoT.Server/Components/Sensors/DhtSensor.cs
@@ -2,6 +2,7 @@
 using FourTwenty.IoT.Connect.Interfaces;
 using Iot.Device.DHTxx;
 using System;
+using System.Collections.Generic;
 using System.Device.Gpio;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,11 @@ namespace FourTwenty.IoT.Server.Components.Sensors
     public class DhtSensor : IoTComponent, ISensor<Dht11>
     {
         private readonly Dht11 _sensor;
+
+        public DhtSensor(int gpioPin, GpioController controller, IReadOnlyCollection<IRule> rules) : base(rules, new[] { gpioPin }, controller)
+        {
+            _sensor = new Dht11(gpioPin);
+        }
         public DhtSensor(int gpioPin, GpioController controller) : base(new[] { gpioPin }, controller)
         {
             _sensor = new Dht11(gpioPin);
